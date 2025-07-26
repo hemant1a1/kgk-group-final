4ca9350b62aeed3f28660a66b3350f6e3b543a6d
revuer <jazz.homi@gmail.com>
2025-07-26 14:51:17 +0530

    update

--- components/Home/Newsletter/index.jsx
+++ components/Home/Newsletter/index.jsx
@@ -22,13 +22,13 @@ export default function Newsletter({
           </p>
         )}
 
-        <div className="flex w-full max-w-md h-[57px] mt-2">
+        <div className="flex flex-col sm:flex-row w-full max-w-md sm:h-[57px] mt-2">
           <input
             type="email"
             placeholder="Enter your E-mail"
-            className="flex-1 px-4 text-white border border-mid-gray border-r-0 h-full focus:outline-none placeholder:text-white placeholder:text-[12px] placeholder:leading-[18px] placeholder:font-light"
+            className="w-full sm:flex-1 px-4 text-white border border-mid-gray sm:border-r-0 h-[57px] focus:outline-none placeholder:text-white placeholder:text-[12px] placeholder:leading-[18px] placeholder:font-light"
           />
-          <button className="h-full px-6 border border-mid-gray bg-mid-gray hover:bg-mid-gray text-[18px] leading-[27px] text-primary uppercase font-normal">
+          <button className="w-full sm:w-auto h-[57px] px-6 border border-mid-gray bg-mid-gray hover:bg-mid-gray text-[18px] leading-[27px] text-primary uppercase font-normal mt-2 sm:mt-0">
             SUBSCRIBE
           </button>
         </div>
