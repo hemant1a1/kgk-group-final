8a7b8c7971d12e7fb6ca79ec8e763ad50f513706
revuer <jazz.homi@gmail.com>
2025-08-07 15:56:40 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['reinventmedia.in'],
+        domains: ['reinventmedia.in', 'img.youtube.com'],
     },
 };
 
