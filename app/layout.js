f18c480f6e9e822219339ea97e5d13d39088afb9
revuer <jazz.homi@gmail.com>
2025-09-02 11:40:26 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,19 @@ export const metadata = {
     index: false,
     follow: false,
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      { rel: "icon", url: "/android-chrome-192x192.png", sizes: "192x192" },
+      { rel: "icon", url: "/android-chrome-512x512.png", sizes: "512x512" },
+    ],
+  },
+  themeColor: "#000000",
 };
 
 export default function RootLayout({ children }) {
