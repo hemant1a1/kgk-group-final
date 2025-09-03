59310ca89e54248da2c8e54ee5fd608e562cb575
revuer <jazz.homi@gmail.com>
2025-09-03 16:21:58 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -3,26 +3,5 @@ const nextConfig = {
     images: {
         domains: ['reinventmedia.in', 'img.youtube.com'],
     },
-
-    // ✅ Optimize fonts
-    experimental: {
-        optimizeCss: true,     // reduce CSS size
-        optimizePackageImports: ['lucide-react', 'lodash'], // replace with your heavy libs
-    },
-
-    // ✅ Long cache headers for static assets
-    async headers() {
-        return [
-        {
-            source: "/:all*(svg|jpg|png|gif|ico|webp|avif)",
-            headers: [
-            {
-                key: "Cache-Control",
-                value: "public, max-age=31536000, immutable",
-            },
-            ],
-        },
-        ];
-    },
 };
 export default nextConfig;
